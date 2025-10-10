18d736b8ee33b367fab12cdc8b5a34fc76011bab
kartikpande123 <kartikpande67@gmail.com>
2025-10-10 16:54:04 +0530

    cors2

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ const crypto = require("crypto");
 
 
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
